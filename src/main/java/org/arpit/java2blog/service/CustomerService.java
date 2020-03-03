80e15b0b3f9e90de0e6ba84da36c08c18f03b1ad
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2020-03-03 07:55:41 +0100

    Customer service created, implemented

---
+++ src/main/java/org/arpit/java2blog/service/CustomerService.java
@@ -0,0 +1,37 @@
+package org.arpit.java2blog.service;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.arpit.java2blog.model.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("customerService")
+public class CustomerService {
+	
+	@Autowired
+	CustomerDao customerDao;
+	
+	@Transactional
+	public List<Customer> getAllCustomers(){
+		return customerDao.getAllCustomers();
+	}
+	@Transactional
+	public Customer getCustomer(int id) {
+		return customerDao.getCustomer(id);
+	}
+	@Transactional
+	public Customer addCustomer(Customer customer) {
+		customerDao.addCustomer(customer);
+	}
+	@Transactional
+	public Customer updateCustomer(Customer customer) {
+		customerDao.updateCustomer(customer);
+	}
+	@Transactional
+	public void deleteCustomer(int id) {
+		customerDao.deleteCustomer(id);
+	}
+}
